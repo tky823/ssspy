7c448e906a79ac3890ea07c7bd4aa4f45077327b
tky823 <tky823.main+github@gmail.com>
2022-05-12 23:10:26 +0900

    [src] Add flooring operation

---
+++ ssspy/bss/_flooring.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+EPS = 1e-12
+
+
+def max_flooring(input: np.ndarray, eps=EPS):
+    r"""Max flooring operation.
+    """
+    return np.maximum(input, eps)
+
+
+def add_flooring(input: np.ndarray, eps=EPS):
+    r"""Add flooring operation.
+    """
+    return input + eps
