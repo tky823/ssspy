22f64e8c35e964fe0c09251ba221101c34abd09f
tky823 <tky823.main+github@gmail.com>
2022-07-02 21:35:44 +0900

    [style] Remove unnecessary comments

--- tests/transform/test_whiten.py
+++ tests/transform/test_whiten.py
@@ -13,14 +13,14 @@ def test_whiten(n_channels: int, n_bins: int, n_frames: int):
     real = np.random.randn(n_channels, n_bins, n_frames)
     imag = np.random.randn(n_channels, n_bins, n_frames)
     input = real + 1j * imag
-    output = whiten(input)  # (n_channels, n_bins, n_frames)
+    output = whiten(input)
 
     assert input.shape == output.shape
 
     covariance = output[:, np.newaxis, :, :] * output[np.newaxis, :, :, :].conj()
-    covariance = np.mean(covariance, axis=-1)  # (n_channels, n_channels, n_bins)
-    covariance = covariance.transpose(2, 0, 1)  # (n_bins, n_channels, n_channels)
-    eye = np.eye(n_channels)  # (n_channels, n_channels)
-    eye = np.tile(eye, reps=(n_bins, 1, 1))  # (n_bins, n_channels, n_channels)
+    covariance = np.mean(covariance, axis=-1)
+    covariance = covariance.transpose(2, 0, 1)
+    eye = np.eye(n_channels)
+    eye = np.tile(eye, reps=(n_bins, 1, 1))
 
     assert np.allclose(covariance, eye)
