a32df448293b14f4332feeaa46e3981b859d8658
tky823 <tky823.main+github@gmail.com>
2022-07-02 21:28:04 +0900

    [test] Add test for whitening

---
+++ tests/transform/test_whiten.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+
+from ssspy.transform import whiten
+
+parameters_whiten = [(2, 2049, 8), (3, 513, 12)]
+
+
+@pytest.mark.parametrize("n_channels, n_bins, n_frames", parameters_whiten)
+def test_whiten(n_channels: int, n_bins: int, n_frames: int):
+    np.random.seed(111)
+
+    real = np.random.randn(n_channels, n_bins, n_frames)
+    imag = np.random.randn(n_channels, n_bins, n_frames)
+    input = real + 1j * imag
+    output = whiten(input)  # (n_channels, n_bins, n_frames)
+
+    assert input.shape == output.shape
+
+    covariance = output[:, np.newaxis, :, :] * output[np.newaxis, :, :, :].conj()
+    covariance = np.mean(covariance, axis=-1)  # (n_channels, n_channels, n_bins)
+    covariance = covariance.transpose(2, 0, 1)  # (n_bins, n_channels, n_channels)
+    eye = np.eye(n_channels)  # (n_channels, n_channels)
+    eye = np.tile(eye, reps=(n_bins, 1, 1))  # (n_bins, n_channels, n_channels)
+
+    assert np.allclose(covariance, eye)
