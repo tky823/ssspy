38bce17a803617fd052c96bfab92b57bf7bb50b1
tky823 <tky823.main+github@gmail.com>
2022-07-07 19:13:06 +0900

    [src] Add pair selector

---
+++ ssspy/bss/_select_pair.py
@@ -0,0 +1,18 @@
+from typing import Iterable
+
+
+def pair_selector(n_sources: int) -> Iterable:
+    r"""Select pair in pairwise update
+
+    Args:
+        n_sources: int
+
+    Yields:
+        int:
+            First element of updating pair
+        int:
+            Seceond element of updating pair
+    """
+    for src_idx in range(n_sources):
+        m, n = 2 * src_idx, 2 * src_idx + 1
+        yield m % n_sources, n % n_sources
