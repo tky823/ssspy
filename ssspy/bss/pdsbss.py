e8733dc9cf61a5ae260c91c94a18de644ed8b0a1
tky823 <tky823.main+github@gmail.com>
2022-07-09 01:36:34 +0900

    [src] Add base class of PDS-BSS

--- ssspy/bss/pdsbss.py
+++ ssspy/bss/pdsbss.py
@@ -1,20 +1,42 @@
 from typing import Optional, Union, List, Callable
-import functools
 
 import numpy as np
 
-from ._flooring import max_flooring
 from ..algorithm import projection_back
 
 EPS = 1e-10
 
 
 class PDSBSSbase:
+    r"""Base class of based on blind source separation \
+    via proximal splitting algorithm [#yatabe2018determined]_.
+
+    Args:
+        prox_penalty (callable):
+            Proximal operator of penalty function.
+            Default: ``None``.
+        callbacks (callable or list[callable], optional):
+            Callback functions. Each function is called before separation and at each iteration.
+            Default: ``None``.
+        should_apply_projection_back (bool):
+            If ``should_apply_projection_back=True``, the projection back is applied to \
+            estimated spectrograms. Default: ``True``.
+        should_record_loss (bool):
+            Record the loss at each iteration of the update algorithm \
+            if ``should_record_loss=True``.
+            Default: ``True``.
+        reference_id (int):
+            Reference channel for projection back.
+            Default: ``0``.
+
+    .. [#yatabe2018determined] K. Yatabe and D. Kitamura,
+        "Determined blind source separation via proximal splitting algorithm,"
+        in *Proc of ICASSP*, pp. 776-780, 2018.
+    """
+
     def __init__(
         self,
-        flooring_fn: Optional[Callable[[np.ndarray], np.ndarray]] = functools.partial(
-            max_flooring, eps=EPS
-        ),
+        prox_penalty: Callable[[np.ndarray, float], np.ndarray] = None,
         callbacks: Optional[
             Union[Callable[["PDSBSSbase"], None], List[Callable[["PDSBSSbase"], None]]]
         ] = None,
@@ -22,10 +44,10 @@ class PDSBSSbase:
         should_record_loss: bool = True,
         reference_id: int = 0,
     ) -> None:
-        if flooring_fn is None:
-            self.flooring_fn = lambda x: x
+        if prox_penalty is None:
+            raise ValueError("Specify proximal operator of penalty function.")
         else:
-            self.flooring_fn = flooring_fn
+            self.prox_penalty = prox_penalty
 
         if callbacks is not None:
             if callable(callbacks):
@@ -33,3 +55,136 @@ class PDSBSSbase:
             self.callbacks = callbacks
         else:
             self.callbacks = None
+
+        self.input = None
+        self.should_apply_projection_back = should_apply_projection_back
+
+        if reference_id is None and should_apply_projection_back:
+            raise ValueError("Specify 'reference_id' if should_apply_projection_back=True.")
+        else:
+            self.reference_id = reference_id
+
+        self.should_record_loss = should_record_loss
+
+        if self.should_record_loss:
+            self.loss = []
+        else:
+            self.loss = None
+
+    def __call__(self, input: np.ndarray, n_iter: int = 100, **kwargs) -> np.ndarray:
+        r"""Separate a frequency-domain multichannel signal.
+
+        Args:
+            input (numpy.ndarray):
+                Mixture signal in frequency-domain.
+                The shape is (n_channels, n_bins, n_frames).
+            n_iter (int):
+                Number of iterations of demixing filter updates.
+                Default: 100.
+
+        Returns:
+            numpy.ndarray:
+                The separated signal in frequency-domain.
+                The shape is (n_channels, n_bins, n_frames).
+        """
+        self.input = input.copy()
+
+        self._reset(**kwargs)
+
+        raise NotImplementedError("Implement '__call__' method.")
+
+    def __repr__(self) -> str:
+        s = "PDSBSS("
+        s += ", should_apply_projection_back={should_apply_projection_back}"
+        s += ", should_record_loss={should_record_loss}"
+
+        if self.should_apply_projection_back:
+            s += ", reference_id={reference_id}"
+
+        s += ")"
+
+        return s.format(**self.__dict__)
+
+    def _reset(self, **kwargs) -> None:
+        r"""Reset attributes following on given keyword arguments.
+
+        Args:
+            kwargs:
+                Set arguments as attributes of PDSBSSbase.
+        """
+        assert self.input is not None, "Specify data!"
+
+        for key in kwargs.keys():
+            setattr(self, key, kwargs[key])
+
+        X = self.input
+
+        n_channels, n_bins, n_frames = X.shape
+        n_sources = n_channels  # n_channels == n_sources
+
+        self.n_sources, self.n_channels = n_sources, n_channels
+        self.n_bins, self.n_frames = n_bins, n_frames
+
+        if not hasattr(self, "demix_filter"):
+            W = np.eye(n_sources, n_channels, dtype=np.complex128)
+            W = np.tile(W, reps=(n_bins, 1, 1))
+        else:
+            if self.demix_filter is None:
+                W = None
+            else:
+                # To avoid overwriting ``demix_filter`` given by keyword arguments.
+                W = self.demix_filter.copy()
+
+        self.demix_filter = W
+        self.output = self.separate(X, demix_filter=W)
+
+    def separate(self, input: np.ndarray, demix_filter: np.ndarray) -> np.ndarray:
+        r"""Separate ``input`` using ``demixing_filter``.
+
+        .. math::
+            \boldsymbol{y}_{ij}
+            = \boldsymbol{W}_{i}\boldsymbol{x}_{ij}
+
+        Args:
+            input (numpy.ndarray):
+                The mixture signal in frequency-domain.
+                The shape is (n_channels, n_bins, n_frames).
+            demix_filter (numpy.ndarray):
+                The demixing filters to separate ``input``.
+                The shape is (n_bins, n_sources, n_channels).
+
+        Returns:
+            numpy.ndarray:
+                The separated signal in frequency-domain.
+                The shape is (n_sources, n_bins, n_frames).
+        """
+        X, W = input, demix_filter
+        Y = W @ X.transpose(1, 0, 2)
+        output = Y.transpose(1, 0, 2)
+
+        return output
+
+    def compute_loss(self) -> float:
+        r"""Compute loss :math:`\mathcal{L}`.
+
+        Returns:
+            float:
+                Computed loss.
+        """
+        raise NotImplementedError("Implement 'compute_loss'.")
+
+    def compute_logdet(self, demix_filter: np.ndarray) -> np.ndarray:
+        r"""Compute log-determinant of demixing filter
+        """
+        return np.log(np.abs(np.linalg.det(demix_filter)))  # (n_bins,)
+
+    def apply_projection_back(self) -> None:
+        r"""Apply projection back technique to estimated spectrograms.
+        """
+        assert self.should_apply_projection_back, "Set self.should_apply_projection_back=True."
+
+        X, W = self.input, self.demix_filter
+        W_scaled = projection_back(W, reference_id=self.reference_id)
+        Y_scaled = self.separate(X, demix_filter=W_scaled)
+
+        self.output, self.demix_filter = Y_scaled, W_scaled
