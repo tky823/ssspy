1ac3f3260082be5954428cb21f502d1e10ba8ec3
tky823 <tky823.main+github@gmail.com>
2022-07-14 21:27:12 +0900

    [src] Sort selecting pair

--- ssspy/bss/_select_pair.py
+++ ssspy/bss/_select_pair.py
@@ -1,12 +1,14 @@
 from typing import Iterable, Tuple
 
 
-def pair_selector(n_sources: int) -> Iterable[Tuple[int, int]]:
+def pair_selector(n_sources: int, sort: bool = True) -> Iterable[Tuple[int, int]]:
     r"""Select pair in pairwise update
 
     Args:
         n_sources (int):
             Number of sources.
+        sort (bool):
+            Sort pair to ensure :math:`m<n` if ``sort = True``.
 
     Yields:
         int:
@@ -16,4 +18,9 @@ def pair_selector(n_sources: int) -> Iterable[Tuple[int, int]]:
     """
     for src_idx in range(n_sources):
         m, n = 2 * src_idx, 2 * src_idx + 1
-        yield m % n_sources, n % n_sources
+        m, n = m % n_sources, n % n_sources
+
+        if sort:
+            m, n = (n, m) if m > n else (m, n)
+
+        yield m, n
