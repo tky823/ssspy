90a4afdbe5e8b7509c6ac8aca53c3815c6190445
tky823 <tky823.main+github@gmail.com>
2022-07-16 01:20:45 +0900

    [tests] Add ascending option to tests

--- tests/transform/test_pca.py
+++ tests/transform/test_pca.py
@@ -3,19 +3,21 @@ import numpy as np
 
 from ssspy.transform import pca
 
+parameters_ascend = [True, False]
 parameters_batch_size = [1, 4]
 parameters_n_channels = [2, 3]
 parameters_pca_real = [10, 20]
 parameters_pca_complex = [(257, 8), (65, 12)]
 
 
+@pytest.mark.parametrize("ascend", parameters_ascend)
 @pytest.mark.parametrize("n_channels", parameters_n_channels)
 @pytest.mark.parametrize("n_samples", parameters_pca_real)
-def test_pca_real_2d(n_channels: int, n_samples: int):
+def test_pca_real_2d(ascend: bool, n_channels: int, n_samples: int):
     np.random.seed(111)
 
     input = np.random.randn(n_channels, n_samples)
-    output = pca(input)
+    output = pca(input, ascend=ascend)
 
     assert input.shape == output.shape
 
@@ -27,14 +29,15 @@ def test_pca_real_2d(n_channels: int, n_samples: int):
     assert np.allclose(mask * covariance, zero)
 
 
+@pytest.mark.parametrize("ascend", parameters_ascend)
 @pytest.mark.parametrize("batch_size", parameters_batch_size)
 @pytest.mark.parametrize("n_channels", parameters_n_channels)
 @pytest.mark.parametrize("n_samples", parameters_pca_real)
-def test_pca_real_3d(batch_size: int, n_channels: int, n_samples: int):
+def test_pca_real_3d(ascend: bool, batch_size: int, n_channels: int, n_samples: int):
     np.random.seed(111)
 
     input = np.random.randn(batch_size, n_channels, n_samples)
-    output = pca(input)
+    output = pca(input, ascend=ascend)
 
     assert input.shape == output.shape
 
@@ -46,15 +49,16 @@ def test_pca_real_3d(batch_size: int, n_channels: int, n_samples: int):
     assert np.allclose(mask * covariance, zero)
 
 
+@pytest.mark.parametrize("ascend", parameters_ascend)
 @pytest.mark.parametrize("n_channels", parameters_n_channels)
 @pytest.mark.parametrize("n_bins, n_frames", parameters_pca_complex)
-def test_pca_complex_3d(n_channels: int, n_bins: int, n_frames: int):
+def test_pca_complex_3d(ascend: bool, n_channels: int, n_bins: int, n_frames: int):
     np.random.seed(111)
 
     real = np.random.randn(n_channels, n_bins, n_frames)
     imag = np.random.randn(n_channels, n_bins, n_frames)
     input = real + 1j * imag
-    output = pca(input)
+    output = pca(input, ascend=ascend)
 
     assert input.shape == output.shape
 
@@ -67,16 +71,17 @@ def test_pca_complex_3d(n_channels: int, n_bins: int, n_frames: int):
     assert np.allclose(mask * covariance, zero)
 
 
+@pytest.mark.parametrize("ascend", parameters_ascend)
 @pytest.mark.parametrize("batch_size", parameters_batch_size)
 @pytest.mark.parametrize("n_channels", parameters_n_channels)
 @pytest.mark.parametrize("n_bins, n_frames", parameters_pca_complex)
-def test_pca_complex_4d(batch_size: int, n_channels: int, n_bins: int, n_frames: int):
+def test_pca_complex_4d(ascend: bool, batch_size: int, n_channels: int, n_bins: int, n_frames: int):
     np.random.seed(111)
 
     real = np.random.randn(batch_size, n_channels, n_bins, n_frames)
     imag = np.random.randn(batch_size, n_channels, n_bins, n_frames)
     input = real + 1j * imag
-    output = pca(input)
+    output = pca(input, ascend=ascend)
 
     assert input.shape == output.shape
 
