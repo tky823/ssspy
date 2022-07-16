e7eb5c1aba218e6cbc8aee8445958287aebc308a
tky823 <tky823.main+github@gmail.com>
2022-07-16 16:02:32 +0900

    [tests] Add tests of t-ILRMA w/ partitioning

--- tests/bss/test_ilrma.py
+++ tests/bss/test_ilrma.py
@@ -138,6 +138,60 @@ def test_gauss_ilrma_wo_latent(
 )
 @pytest.mark.parametrize("dof", parameters_dof)
 @pytest.mark.parametrize("callbacks", parameters_callbacks)
+@pytest.mark.parametrize("normalization", parameters_normalization_latent)
+def test_t_ilrma_latent(
+    n_sources: int,
+    n_basis: int,
+    dof: float,
+    domain: float,
+    callbacks: Optional[Union[Callable[[GaussILRMA], None], List[Callable[[GaussILRMA], None]]]],
+    normalization: Optional[Union[str, bool]],
+):
+    np.random.seed(111)
+
+    algorithm_spatial = "IP1"
+
+    if n_sources < 4:
+        sisec2010_tag = "dev1_female3"
+    elif n_sources == 4:
+        sisec2010_tag = "dev1_female4"
+    else:
+        raise ValueError("n_sources should be less than 5.")
+
+    waveform_src_img = download_dummy_data(
+        sisec2010_root=sisec2010_root,
+        mird_root=mird_root,
+        n_sources=n_sources,
+        sisec2010_tag=sisec2010_tag,
+        max_samples=max_samples,
+    )
+    waveform_mix = np.sum(waveform_src_img, axis=1)  # (n_channels, n_samples)
+
+    _, _, spectrogram_mix = ss.stft(
+        waveform_mix, window="hann", nperseg=n_fft, noverlap=n_fft - hop_length
+    )
+
+    ilrma = TILRMA(
+        n_basis,
+        dof=dof,
+        algorithm_spatial=algorithm_spatial,
+        domain=domain,
+        partitioning=True,
+        callbacks=callbacks,
+        normalization=normalization,
+    )
+    spectrogram_est = ilrma(spectrogram_mix, n_iter=n_iter)
+
+    assert spectrogram_mix.shape == spectrogram_est.shape
+
+    print(ilrma)
+
+
+@pytest.mark.parametrize(
+    "n_sources, n_basis, domain", parameters_gauss_ilrma_wo_latent,
+)
+@pytest.mark.parametrize("dof", parameters_dof)
+@pytest.mark.parametrize("callbacks", parameters_callbacks)
 @pytest.mark.parametrize("normalization", parameters_normalization_wo_latent)
 def test_t_ilrma_wo_latent(
     n_sources: int,
