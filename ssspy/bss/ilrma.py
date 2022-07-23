3fae2a7c8fab4b432f1dbed97fc59c8017f5b9de
tky823 <tky823.main+github@gmail.com>
2022-07-23 17:23:13 +0900

    [src] Add GGDILRMA-ISS2

--- ssspy/bss/ilrma.py
+++ ssspy/bss/ilrma.py
@@ -2250,6 +2250,8 @@ class GGDILRMA(ILRMAbase):
             self.update_spatial_model_ip2()
         elif self.algorithm_spatial in ["ISS", "ISS1"]:
             self.update_spatial_model_iss1()
+        elif self.algorithm_spatial in ["ISS2"]:
+            self.update_spatial_model_iss2()
         else:
             raise NotImplementedError("Not support {}.".format(self.algorithm_spatial))
 
@@ -2484,6 +2486,152 @@ class GGDILRMA(ILRMAbase):
             v_n[src_idx] = 1 - 1 / np.sqrt((beta / 2) * denom[src_idx])
 
             Y = Y - v_n[:, :, np.newaxis] * Y_n
+
+        self.output = Y
+
+    def update_spatial_model_iss2(self) -> None:
+        r"""Update estimated spectrograms once using pairwise iterative source steering.
+
+        Then, we compute :math:`\boldsymbol{G}_{in}^{(m,m')}` \
+        and :math:`\boldsymbol{f}_{in}^{(m,m')}` for :math:`m\neq m'`:
+
+        .. math::
+            \begin{array}{rclc}
+                \boldsymbol{G}_{in}^{(m,m')}
+                &=& {\displaystyle\frac{1}{J}\sum_{j}}\frac{1}{\tilde{r}_{ijn}}
+                \boldsymbol{y}_{ij}^{(m,m')}{\boldsymbol{y}_{ij}^{(m,m')}}^{\mathsf{H}}
+                &(n=1,\ldots,N), \\
+                \boldsymbol{f}_{in}^{(m,m')}
+                &=& {\displaystyle\frac{1}{J}\sum_{j}}
+                \frac{1}{\tilde{r}_{ijn}}y_{ijn}^{*}\boldsymbol{y}_{ij}^{(m,m')}
+                &(n\neq m,m'),
+            \end{array}
+
+        where
+
+        .. math::
+            \tilde{r}_{ijn}
+            = \frac{2}{\beta}|y_{ijn}|^{2-\beta}
+            \left(\sum_{k}z_{nk}t_{ik}v_{kj}\right)^{\frac{\beta}{p}},
+
+        if ``partitioning=True``.
+        Otherwise,
+
+        .. math::
+            \tilde{r}_{ijn}
+            = \frac{2}{\beta}|y_{ijn}|^{2-\beta}
+            \left(\sum_{k}t_{ikn}v_{kjn}\right)^{\frac{\beta}{p}}.
+
+        Using :math:`\boldsymbol{G}_{in}^{(m,m')}` and :math:`\boldsymbol{f}_{in}`, \
+        we compute
+
+        .. math::
+            \begin{array}{rclc}
+                \boldsymbol{p}_{in}
+                &=& \dfrac{\boldsymbol{h}_{in}}
+                {\sqrt{\boldsymbol{h}_{in}^{\mathsf{H}}\boldsymbol{G}_{in}^{(m,m')}
+                \boldsymbol{h}_{in}}} & (n=m,m'), \\
+                \boldsymbol{q}_{in}
+                &=& -{\boldsymbol{G}_{in}^{(m,m')}}^{-1}\boldsymbol{f}_{in}^{(m,m')}
+                & (n\neq m,m'),
+            \end{array}
+
+        where :math:`\boldsymbol{h}_{in}` (:math:`n=m,m'`) is \
+        a generalized eigenvector obtained from
+
+        .. math::
+            \boldsymbol{G}_{im}^{(m,m')}\boldsymbol{h}_{i}
+            = \lambda_{i}\boldsymbol{G}_{im'}^{(m,m')}\boldsymbol{h}_{i}.
+
+        Separated signal :math:`y_{ijn}` is updated as follows:
+
+        .. math::
+            y_{ijn}
+            &\leftarrow\begin{cases}
+            &\boldsymbol{p}_{in}^{\mathsf{H}}\boldsymbol{y}_{ij}^{(m,m')} & (n=m,m') \\
+            &\boldsymbol{q}_{in}^{\mathsf{H}}\boldsymbol{y}_{ij}^{(m,m')} + y_{ijn} & (n\neq m,m')
+            \end{cases}.
+        """
+        n_sources = self.n_sources
+
+        p = self.domain
+        beta = self.beta
+
+        Y = self.output
+
+        Y2b = np.abs(Y) ** (2 - beta)
+        Y2b = self.flooring_fn(Y2b)
+
+        if self.partitioning:
+            Z = self.latent
+            T, V = self.basis, self.activation
+
+            ZTV = self.reconstruct_nmf(T, V, latent=Z)
+            ZTVbp = ZTV ** (beta / p)
+            R_tilde = (2 / beta) * Y2b * ZTVbp
+        else:
+            T, V = self.basis, self.activation
+
+            TV = self.reconstruct_nmf(T, V)
+            TVbp = TV ** (beta / p)
+            R_tilde = (2 / beta) * Y2b * TVbp
+
+        varphi = 1 / R_tilde
+
+        for m, n in self.pair_selector(n_sources):
+            # Split into main and sub
+            Y_1, Y_m, Y_2, Y_n, Y_3 = np.split(Y, [m, m + 1, n, n + 1], axis=0)
+            Y_main = np.concatenate([Y_m, Y_n], axis=0)  # (2, n_bins, n_frames)
+            Y_sub = np.concatenate([Y_1, Y_2, Y_3], axis=0)  # (n_sources - 2, n_bins, n_frames)
+
+            varphi_1, varphi_m, varphi_2, varphi_n, varphi_3 = np.split(
+                varphi, [m, m + 1, n, n + 1], axis=0
+            )
+            varphi_main = np.concatenate([varphi_m, varphi_n], axis=0)
+            varphi_sub = np.concatenate([varphi_1, varphi_2, varphi_3], axis=0)
+
+            YY_main = Y_main[:, np.newaxis, :, :] * Y_main[np.newaxis, :, :, :].conj()
+            YY_sub = Y_main[:, np.newaxis, :, :] * Y_sub[np.newaxis, :, :, :].conj()
+            YY_main = YY_main.transpose(2, 0, 1, 3)
+            YY_sub = YY_sub.transpose(1, 2, 0, 3)
+
+            Y_main = Y_main.transpose(1, 0, 2)
+
+            # Sub
+            G_sub = np.mean(
+                varphi_sub[:, :, np.newaxis, np.newaxis, :] * YY_main[np.newaxis, :, :, :, :],
+                axis=-1,
+            )
+            F = np.mean(varphi_sub[:, :, np.newaxis, :] * YY_sub, axis=-1)
+            Q = -np.linalg.inv(G_sub) @ F[:, :, :, np.newaxis]
+            Q = Q.squeeze(axis=-1)
+            Q = Q.transpose(1, 0, 2)
+            QY = Q.conj() @ Y_main
+            Y_sub = Y_sub + QY.transpose(1, 0, 2)
+
+            # Main
+            G_main = np.mean(
+                varphi_main[:, :, np.newaxis, np.newaxis, :] * YY_main[np.newaxis, :, :, :, :],
+                axis=-1,
+            )
+            G_m, G_n = G_main
+            _, H_mn = eigh(G_m, G_n)
+            h_mn = H_mn.transpose(2, 0, 1)
+            hGh_mn = h_mn[:, :, np.newaxis, :].conj() @ G_main @ h_mn[:, :, :, np.newaxis]
+            hGh_mn = np.squeeze(hGh_mn, axis=-1)
+            hGh_mn = np.real(hGh_mn)
+            hGh_mn = np.maximum(hGh_mn, 0)
+            denom_mn = np.sqrt(hGh_mn)
+            denom_mn = self.flooring_fn(denom_mn)
+            P = h_mn / denom_mn
+            P = P.transpose(1, 0, 2)
+            Y_main = P.conj() @ Y_main
+            Y_main = Y_main.transpose(1, 0, 2)
+
+            # Concat
+            Y_m, Y_n = np.split(Y_main, [1], axis=0)
+            Y1, Y2, Y3 = np.split(Y_sub, [m, n - 1], axis=0)
+            Y = np.concatenate([Y1, Y_m, Y2, Y_n, Y3], axis=0)
 
         self.output = Y
 
