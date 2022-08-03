128be00ca835341d7245cd77c88fe35e937393c5
tky823 <tky823.main+github@gmail.com>
2022-08-04 01:15:12 +0900

    [src] Add 2x2 matrix inversion

---
+++ ssspy/linalg/inv.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def inv2(X: np.ndarray) -> np.ndarray:
+    r"""Compute inversion of 2x2 matrix.
+
+    Args:
+        X (numpy.ndarray):
+            Complex matrix with shape of (*, 2, 2).
+
+    Returns:
+        numpy.ndarray:
+            Inverse matrix of X whose shape is (*, 2, 2).
+    """
+    shape = X.shape
+
+    assert shape[-2:] == (2, 2), "2x2 matrix is expected, but given shape of {}.".format(shape)
+
+    a = X[..., 0, 0]
+    b = X[..., 0, 1]
+    c = X[..., 1, 0]
+    d = X[..., 1, 1]
+
+    det = a * d - b * c
+
+    X_adj = np.stack([d, -b, -c, a], axis=-1)
+    X_adj = X_adj.reshape(shape[:-1] + (2, 2))
+    X_inv = X_adj / det[..., None, None]
+
+    return X_inv
