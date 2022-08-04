548182d982ed196e1db8803d64a210aae9a25486
tky823 <tky823.main+github@gmail.com>
2022-08-05 00:51:36 +0900

    [src] Use np.linalg.eigh

--- ssspy/linalg/eigh.py
+++ ssspy/linalg/eigh.py
@@ -60,32 +60,7 @@ def eigh2(A: np.ndarray, B: np.ndarray = None) -> np.ndarray:
     assert A.shape[-2:] == (2, 2), "2x2 matrix is expected, but given shape of {}.".format(A.shape)
 
     if B is None:
-        a = A[..., 0, 0]
-        b = A[..., 0, 1]
-        c = A[..., 1, 0]
-        d = A[..., 1, 1]
-
-        if np.iscomplexobj(A):
-            a = np.real(a)
-            d = np.real(d)
-
-            assert np.allclose(c.conj(), b), "Matrix should be positive semidefinite."
-        else:
-            assert np.allclose(c, b), "Matrix should be positive semidefinite."
-
-        trace = a + d
-        det = a * d - np.abs(b) ** 2
-        sqrt_discriminant = np.sqrt(trace ** 2 - 4 * det)
-        lamb_pos = trace + sqrt_discriminant
-        lamb_neg = trace - sqrt_discriminant
-        v_00 = (d - a - sqrt_discriminant) / 2
-        v_pos = np.stack([v_00, -c], axis=-1)
-        v_neg = np.stack([-b, -v_00], axis=-1)
-
-        lamb = np.stack([lamb_neg, lamb_pos], axis=-1) / 2
-        v = np.stack([v_neg, v_pos], axis=-1)
-
-        return lamb, v
+        return np.linalg.eigh(A)
 
     L = np.linalg.cholesky(B)
     L_inv = inv2(L)
