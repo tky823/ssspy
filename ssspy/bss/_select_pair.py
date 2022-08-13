48cbfd0a21a879702bee49868b5336d36b568eb0
tky823 <tky823.main+github@gmail.com>
2022-08-13 23:50:08 +0900

    [src] Set step parameter in pairwise update

--- ssspy/bss/_select_pair.py
+++ ssspy/bss/_select_pair.py
@@ -2,12 +2,19 @@ from typing import Iterable, Tuple
 import itertools
 
 
-def sequential_pair_selector(n_sources: int, sort: bool = False) -> Iterable[Tuple[int, int]]:
+def sequential_pair_selector(
+    n_sources: int, step: int = 1, sort: bool = False
+) -> Iterable[Tuple[int, int]]:
     r"""Select pair in pairwise update.
 
     Args:
         n_sources (int):
             Number of sources.
+        step (int):
+            This parameter determines step size. \
+            For instance, if `sequential_pair_selector(n_sources=6, step=2, sort=False)`, \
+            this function yields `0, 1`, `2, 3`, `4, 5`, `0, 1`, `2, 3`, `4, 5`. \
+            Default: `1`.
         sort (bool):
             Sort pair to ensure :math:`m<n` if ``sort=True``. \
             Default: ``False``.
@@ -18,7 +25,7 @@ def sequential_pair_selector(n_sources: int, sort: bool = False) -> Iterable[Tup
         int:
             Second element of updating pair.
     """
-    for m in range(n_sources):
+    for m in range(0, step * n_sources, step):
         m, n = m % n_sources, (m + 1) % n_sources
 
         if sort:
--- tests/bss/test_select_pair.py
+++ tests/bss/test_select_pair.py
@@ -3,13 +3,15 @@ import pytest
 from ssspy.bss._select_pair import sequential_pair_selector, combination_pair_selector
 
 parameters_n_sources = [2, 3, 4]
+parameters_step = [1, 2]
 parameters_ascend = [True, False]
 
 
 @pytest.mark.parametrize("n_sources", parameters_n_sources)
+@pytest.mark.parametrize("step", parameters_step)
 @pytest.mark.parametrize("ascend", parameters_ascend)
-def test_sequential_pair_selector(n_sources: int, ascend: bool):
-    for m, n in sequential_pair_selector(n_sources, sort=ascend):
+def test_sequential_pair_selector(n_sources: int, step: int, ascend: bool):
+    for m, n in sequential_pair_selector(n_sources, step=step, sort=ascend):
         if ascend:
             assert m < n
 
