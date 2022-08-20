dc949314b7f10fc6f70952e0d01a42549c3b5522
tky823 <tky823.main+github@gmail.com>
2022-08-20 23:16:26 +0900

    [docs] Add notes of eigh

--- ssspy/linalg/eigh.py
+++ ssspy/linalg/eigh.py
@@ -8,10 +8,13 @@ from .inv import inv2
 def eigh(
     A: np.ndarray, B: Optional[np.ndarray] = None
 ) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
-    r"""Generalized eigenvalue decomposition.
+    r"""Compute the (generalized) eigenvalues and eigenvectors of a complex Hermitian \
+    (conjugate symmetric) or a real symmetric matrix.
 
-    Solve :math:`\boldsymbol{A}\boldsymbol{z} = \lambda\boldsymbol{B}\boldsymbol{z}`, \
-    and return :math:`\boldsymbol{z}`.
+    If ``B`` is ``None``, solve :math:`\boldsymbol{A}\boldsymbol{z} = \lambda\boldsymbol{z}`.
+
+    If ``B`` is given, \
+    solve :math:`\boldsymbol{A}\boldsymbol{z} = \lambda\boldsymbol{B}\boldsymbol{z}`.
 
     Args:
         A (numpy.ndarray):
@@ -23,6 +26,16 @@ def eigh(
         A tuple of (eigenvalues, eigenvectors)
             - Eigenvalues have shape of (\*, n_channels).
             - Eigenvectors have shape of (\*, n_channels, n_channels).
+
+    .. note::
+        If ``B`` is given, we use cholesky decomposition to \
+        satisfy :math:`\boldsymbol{L}\boldsymbol{L}^{\mathsf{H}}=\boldsymbol{B}`.
+
+        Then, solve :math:`\boldsymbol{C}\boldsymbol{y} = \lambda\boldsymbol{y}`, \
+        where :math:`\boldsymbol{C}=\boldsymbol{L}^{-1}\boldsymbol{A}\boldsymbol{L}^{-\mathsf{H}}`.
+
+        The generalized eigenvalues of :math:`\boldsymbol{A}` and :math:`\boldsymbol{B}` \
+        are computed by :math:`\boldsymbol{L}^{-\mathsf{H}}\boldsymbol{y}`.
     """
     if B is None:
         return np.linalg.eigh(A)
@@ -45,11 +58,13 @@ def eigh(
 def eigh2(
     A: np.ndarray, B: Optional[np.ndarray] = None
 ) -> Union[np.ndarray, Tuple[np.ndarray, np.ndarray]]:
-    r"""Generalized eigenvalue decomposition for 2x2 matrix.
+    r"""Compute the (generalized) eigenvalues and eigenvectors of a 2x2 complex Hermitian \
+    (conjugate symmetric) or a real symmetric matrix.
 
-    Solve :math:`\boldsymbol{A}\boldsymbol{z} = \lambda\boldsymbol{B}\boldsymbol{z}`, \
-    and return :math:`\boldsymbol{z}`.
-    See also https://github.com/tky823/ssspy/issues/115 for this implementation.
+    If ``B`` is ``None``, solve :math:`\boldsymbol{A}\boldsymbol{z} = \lambda\boldsymbol{z}`.
+
+    If ``B`` is given, \
+    solve :math:`\boldsymbol{A}\boldsymbol{z} = \lambda\boldsymbol{B}\boldsymbol{z}`.
 
     Args:
         A (numpy.ndarray):
@@ -61,6 +76,18 @@ def eigh2(
         A tuple of (eigenvalues, eigenvectors)
             - Eigenvalues have shape of (\*, 2).
             - Eigenvectors have shape of (\*, 2, 2).
+
+    .. note::
+        If ``B`` is given, we use cholesky decomposition to \
+        satisfy :math:`\boldsymbol{L}\boldsymbol{L}^{\mathsf{H}}=\boldsymbol{B}`.
+
+        Then, solve :math:`\boldsymbol{C}\boldsymbol{y} = \lambda\boldsymbol{y}`, \
+        where :math:`\boldsymbol{C}=\boldsymbol{L}^{-1}\boldsymbol{A}\boldsymbol{L}^{-\mathsf{H}}`.
+
+        The generalized eigenvalues of :math:`\boldsymbol{A}` and :math:`\boldsymbol{B}` \
+        are computed by :math:`\boldsymbol{L}^{-\mathsf{H}}\boldsymbol{y}`.
+
+        See also https://github.com/tky823/ssspy/issues/115 for this implementation.
     """
     assert A.shape[-2:] == (2, 2), "2x2 matrix is expected, but given shape of {}.".format(A.shape)
 
