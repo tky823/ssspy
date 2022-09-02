aeafc9aa81acfd5458b61e7bfd6781dcc2fdd537
tky823 <tky823.main+github@gmail.com>
2022-09-03 00:01:54 +0900

    [style] Add type annotations to flooring functions

--- ssspy/bss/_flooring.py
+++ ssspy/bss/_flooring.py
@@ -3,11 +3,11 @@ import numpy as np
 EPS = 1e-12
 
 
-def max_flooring(input: np.ndarray, eps=EPS):
+def max_flooring(input: np.ndarray, eps: float = EPS) -> np.ndarray:
     r"""Max flooring operation."""
     return np.maximum(input, eps)
 
 
-def add_flooring(input: np.ndarray, eps=EPS):
+def add_flooring(input: np.ndarray, eps: float = EPS) -> np.ndarray:
     r"""Add flooring operation."""
     return input + eps
