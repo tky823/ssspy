fae770325f367db3059649613779c399f3231403
tky823 <tky823.main+github@gmail.com>
2022-09-03 09:34:20 +0900

    [src] Implement identity function

--- ssspy/bss/_flooring.py
+++ ssspy/bss/_flooring.py
@@ -3,6 +3,11 @@ import numpy as np
 EPS = 1e-12
 
 
+def identity(input: np.ndarray) -> np.ndarray:
+    r"""Identity function."""
+    return input
+
+
 def max_flooring(input: np.ndarray, eps: float = EPS) -> np.ndarray:
     r"""Max flooring operation."""
     return np.maximum(input, eps)
