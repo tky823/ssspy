db66cec1a833d36e53826664a1dbf4fbc12a99f4
tky823 <tky823.main+github@gmail.com>
2022-10-30 10:27:27 +0900

    [docs] Improve docs of _reset in FastICAbase

--- ssspy/bss/ica.py
+++ ssspy/bss/ica.py
@@ -275,11 +275,11 @@ class FastICAbase(IterativeMethodBase):
         return s.format(**self.__dict__)
 
     def _reset(self, **kwargs) -> None:
-        r"""Reset attributes following on given keyword arguments.
+        r"""Reset attributes by given keyword arguments.
 
         Args:
             kwargs:
-                Set arguments as attributes of ICA.
+                Keyword arguments to set as attributes of ICA.
         """
         assert self.input is not None, "Specify data!"
 
