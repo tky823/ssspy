9e917e8dced660ab9d06e6d413829b60969558e2
tky823 <tky823.main+github@gmail.com>
2022-10-30 19:20:07 +0900

    [docs] Remove unnecessary quotation

--- ssspy/bss/fdica.py
+++ ssspy/bss/fdica.py
@@ -228,7 +228,7 @@ class FDICAbase(IterativeMethodBase):
     def restore_scale(self) -> None:
         r"""Restore scale ambiguity.
 
-        If ``self.scale_restoration="projection_back``, we use projection back technique.
+        If ``self.scale_restoration=projection_back``, we use projection back technique.
         """
         scale_restoration = self.scale_restoration
 
