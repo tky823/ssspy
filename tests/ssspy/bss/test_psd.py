453c8e08d3cc45d8ed1cac4cac9b03e464016c14
tky823 <tky823.main+github@gmail.com>
2022-11-02 00:15:40 +0900

    [tests] Add tests using custom flooring function

--- tests/ssspy/bss/test_psd.py
+++ tests/ssspy/bss/test_psd.py
@@ -3,18 +3,21 @@ from typing import Tuple
 import numpy as np
 import pytest
 
+from ssspy.bss._flooring import add_flooring
 from ssspy.bss._psd import to_psd
 
 rng = np.random.default_rng(42)
 
 parameters_shape = [(5, 2, 2), (3, 3)]
+parameters_kwargs = [{}, {"flooring_fn": None}, {"flooring_fn": add_flooring}]
 
 
 @pytest.mark.parametrize("shape", parameters_shape)
-def test_to_psd_real(shape: Tuple[int]):
+@pytest.mark.parametrize("kwargs", parameters_kwargs)
+def test_to_psd_real(shape: Tuple[int], kwargs):
     X = rng.standard_normal(shape)
     X = X @ X.swapaxes(-1, -2)
-    X = to_psd(X)
+    X = to_psd(X, **kwargs)
     eigvals = np.linalg.eigvalsh(X)
 
     assert np.all(X == X.swapaxes(-1, -2))
@@ -22,10 +25,11 @@ def test_to_psd_real(shape: Tuple[int]):
 
 
 @pytest.mark.parametrize("shape", parameters_shape)
-def test_to_psd_complex(shape: Tuple[int]):
+@pytest.mark.parametrize("kwargs", parameters_kwargs)
+def test_to_psd_complex(shape: Tuple[int], kwargs):
     X = rng.standard_normal(shape) + 1j * rng.standard_normal(shape)
     X = X @ X.swapaxes(-1, -2).conj()
-    X = to_psd(X)
+    X = to_psd(X, **kwargs)
     eigvals = np.linalg.eigvalsh(X)
 
     assert np.all(X == X.swapaxes(-1, -2).conj())
