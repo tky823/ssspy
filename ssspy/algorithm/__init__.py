610d3beec3b62d940409f99afec915c4c7cda83e
tky823 <tky823.main+github@gmail.com>
2022-12-05 01:51:49 +0900

    [src] Define keywords for MDP

--- ssspy/algorithm/__init__.py
+++ ssspy/algorithm/__init__.py
@@ -2,3 +2,9 @@ from .minimal_distortion_principle import minimal_distortion_principle
 from .projection_back import projection_back
 
 __all__ = ["minimal_distortion_principle", "projection_back"]
+
+MINIMAL_DISTORTION_PRINCIPLE_KEYWORDS = [
+    "minimal_distortion_principle",
+    "minimal-distortion-principle",
+    "MDP",
+]
