666ee985775fbbd5b4b0518975142c82f82b2c72
tky823 <tky823.main+github@gmail.com>
2022-12-11 20:47:02 +0900

    [src] Add invsqrtmh to linalg

--- ssspy/linalg/sqrtm.py
+++ ssspy/linalg/sqrtm.py
@@ -1,14 +1,16 @@
+from typing import Callable, Optional
+
 import numpy as np
 
 from .eigh import eigh
 
 
 def sqrtmh(X: np.ndarray) -> np.ndarray:
-    r"""Compute square root of a complex Hermitian (conjugate symmetric) or a real symmetric matrix.
+    r"""Compute square root of a positive semidefinite Hermitian or symmetric matrix.
 
     Args:
         X (numpy.ndarray):
-            A complex Hermitian matrix with shape of (\*, n_channels, n_channels).
+            A complex Hermitian or symmetric matrix with shape of (\*, n_channels, n_channels).
 
     Returns:
         numpy.ndarray of square root. The shape is same as that of input.
@@ -21,5 +23,38 @@ def sqrtmh(X: np.ndarray) -> np.ndarray:
         P_Hermite = P_Hermite.conj()
 
     Lamb = np.sqrt(Lamb)[..., np.newaxis] * np.eye(Lamb.shape[-1])
+
+    return P @ Lamb @ P_Hermite
+
+
+def invsqrtmh(
+    X: np.ndarray,
+    flooring_fn: Optional[Callable[[np.ndarray], np.ndarray]] = None,
+) -> np.ndarray:
+    r"""Compute inversion of square root for a positive definite Hermitian or symmetric matrix.
+
+    Args:
+        X (numpy.ndarray):
+            A complex Hermitian matrix with shape of (\*, n_channels, n_channels).
+
+    Returns:
+        numpy.ndarray of inversion of square root. The shape is same as that of input.
+    """
+
+    def _identity(x):
+        return x
+
+    if flooring_fn is None:
+        flooring_fn = _identity
+
+    Lamb, P = eigh(X)
+
+    P_Hermite = P.swapaxes(-2, -1)
+
+    if np.iscomplexobj(X):
+        P_Hermite = P_Hermite.conj()
+
+    Lamb = 1 / flooring_fn(np.sqrt(Lamb))
+    Lamb = Lamb[..., np.newaxis] * np.eye(Lamb.shape[-1])
 
     return P @ Lamb @ P_Hermite
