b074d36201df22efeb70b7805c7c319f516ab5c9
tky823 <tky823.main+github@gmail.com>
2022-12-18 23:20:56 +0900

    [tests] Add tests for t-IPSDTA

--- tests/ssspy/bss/test_ipsdta.py
+++ tests/ssspy/bss/test_ipsdta.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 import scipy.signal as ss
 
-from ssspy.bss.ipsdta import BlockDecompositionIPSDTAbase, GaussIPSDTA, IPSDTAbase
+from ssspy.bss.ipsdta import TIPSDTA, BlockDecompositionIPSDTAbase, GaussIPSDTA, IPSDTAbase
 
 ssspy_tests_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append(ssspy_tests_dir)
@@ -22,6 +22,7 @@ n_bins = n_fft // 2 + 1
 n_iter = 3
 rng = np.random.default_rng(42)
 
+parameters_dof = [100]
 parameters_spatial_algorithm = ["FPI", "VCD"]
 parameters_callbacks = [None, dummy_function, [DummyCallback(), dummy_function]]
 parameters_source_normalization = [True, False]
@@ -148,6 +149,70 @@ def test_gauss_ipsdta(
             spectrogram_est = ipsdta(spectrogram_mix, n_iter=n_iter, **reset_kwargs)
 
         assert str(e.value) == "IPSDTA with fixed-point iteration is not supported."
+    else:
+        spectrogram_est = ipsdta(spectrogram_mix, n_iter=n_iter, **reset_kwargs)
+
+        assert spectrogram_mix.shape == spectrogram_est.shape
+        assert type(ipsdta.loss[-1]) is float
+
+    print(ipsdta)
+
+
+@pytest.mark.parametrize(
+    "n_sources, n_basis, n_blocks, reset_kwargs",
+    parameters_ipsdta,
+)
+@pytest.mark.parametrize("dof", parameters_dof)
+@pytest.mark.parametrize("spatial_algorithm", parameters_spatial_algorithm)
+@pytest.mark.parametrize("callbacks", parameters_callbacks)
+@pytest.mark.parametrize("source_normalization", parameters_source_normalization)
+@pytest.mark.parametrize("scale_restoration", parameters_scale_restoration)
+def test_t_ipsdta(
+    n_sources: int,
+    n_basis: int,
+    n_blocks: int,
+    dof: float,
+    spatial_algorithm: str,
+    callbacks: Optional[Union[Callable[[GaussIPSDTA], None], List[Callable[[GaussIPSDTA], None]]]],
+    source_normalization: Optional[Union[str, bool]],
+    scale_restoration: Union[str, bool],
+    reset_kwargs: Dict[str, Any],
+):
+    if n_sources < 4:
+        sisec2010_tag = "dev1_female3"
+    elif n_sources == 4:
+        sisec2010_tag = "dev1_female4"
+    else:
+        raise ValueError("n_sources should be less than 5.")
+
+    waveform_src_img, _ = download_sample_speech_data(
+        n_sources=n_sources,
+        sisec2010_tag=sisec2010_tag,
+        max_duration=max_duration,
+        conv=True,
+    )
+    waveform_mix = np.sum(waveform_src_img, axis=1)  # (n_channels, n_samples)
+
+    _, _, spectrogram_mix = ss.stft(
+        waveform_mix, window=window, nperseg=n_fft, noverlap=n_fft - hop_length
+    )
+
+    ipsdta = TIPSDTA(
+        n_basis,
+        n_blocks,
+        dof=dof,
+        spatial_algorithm=spatial_algorithm,
+        callbacks=callbacks,
+        source_normalization=source_normalization,
+        scale_restoration=scale_restoration,
+        rng=rng,
+    )
+
+    if spatial_algorithm != "VCD":
+        with pytest.raises(NotImplementedError) as e:
+            spectrogram_est = ipsdta(spectrogram_mix, n_iter=n_iter, **reset_kwargs)
+
+        assert str(e.value) == "Not support {}.".format(spatial_algorithm)
     else:
         spectrogram_est = ipsdta(spectrogram_mix, n_iter=n_iter, **reset_kwargs)
 
