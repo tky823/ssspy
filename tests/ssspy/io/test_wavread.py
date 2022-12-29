45ffa860c46df007d1294f9209fbbacf1a1e06d7
tky823 <tky823.main+github@gmail.com>
2022-12-29 13:39:56 +0900

    [tests] Add test of waveread for invalid metadata

--- tests/ssspy/io/test_wavread.py
+++ tests/ssspy/io/test_wavread.py
@@ -1,8 +1,16 @@
+import os
+import sys
+
 import numpy as np
 import pytest
 from scipy.io import wavfile
 
 from ssspy import wavread
+
+ssspy_tests_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(ssspy_tests_dir)
+
+from dummy.io import save_invalid_wavfile  # noqa: E402
 
 parameters_frame_offset = [0, 10]
 parameters_num_frames = [None, 100]
@@ -76,13 +84,15 @@ def test_wavread_stereo(frame_offset: int, num_frames: int, channels_first: bool
 
 @pytest.mark.parametrize("frame_offset", parameters_frame_offset)
 def test_wavread_invalid_monoral(frame_offset: int):
-    # invalid memory size
     filename = "./tests/mock/audio/monoral_16k_5sec.wav"
     max_frame = 5 * 16000
     valid_num_frames = max_frame - frame_offset
-    invalid_num_frames = valid_num_frames + 1
 
+    # valid data
     wavread(filename, frame_offset=frame_offset, num_frames=valid_num_frames)
+
+    # invalid memory size
+    invalid_num_frames = valid_num_frames + 1
 
     with pytest.raises(ValueError) as e:
         wavread(filename, frame_offset=frame_offset, num_frames=invalid_num_frames)
@@ -92,15 +102,79 @@ def test_wavread_invalid_monoral(frame_offset: int):
 
 @pytest.mark.parametrize("frame_offset", parameters_frame_offset)
 def test_wavread_invalid_stereo(frame_offset: int):
-    # invalid memory size
     filename = "./tests/mock/audio/stereo_16k_5sec.wav"
     max_frame = 5 * 16000
     valid_num_frames = max_frame - frame_offset
-    invalid_num_frames = valid_num_frames + 1
 
+    # valid data
     wavread(filename, frame_offset=frame_offset, num_frames=valid_num_frames)
+
+    # invalid memory size
+    invalid_num_frames = valid_num_frames + 1
 
     with pytest.raises(ValueError) as e:
         wavread(filename, frame_offset=frame_offset, num_frames=invalid_num_frames)
 
     assert str(e.value) == f"num_frames={invalid_num_frames} exceeds maximum frame {max_frame}."
+
+
+def test_waveread_invalid_metadata():
+    filename = "./tests/mock/audio/monoral_16k_5sec_invalid.wav"
+
+    # invalid riff
+    save_invalid_wavfile(filename, invalid_riff=True)
+
+    with pytest.raises(NotImplementedError) as e:
+        wavread(filename)
+
+    assert str(e.value) == f"Not support {b'RIFX'}."
+
+    # invalid ftype
+    save_invalid_wavfile(filename, invalid_ftype=True)
+
+    with pytest.raises(NotImplementedError) as e:
+        wavread(filename)
+
+    assert str(e.value) == f"Not support {b'wave'}."
+
+    # invalid fmt chunk marker
+    save_invalid_wavfile(filename, invalid_fmt_chunk_marker=True)
+
+    with pytest.raises(NotImplementedError) as e:
+        wavread(filename)
+
+    assert str(e.value) == f"Not support {b'FMT '}."
+
+    # invalid fmt chunk size
+    save_invalid_wavfile(filename, invalid_fmt_chunk_size=True)
+
+    with pytest.raises(NotImplementedError) as e:
+        wavread(filename)
+
+    assert str(e.value) == "Invalid header is detected."
+
+    # invalid fmt
+    save_invalid_wavfile(filename, invalid_fmt=True)
+
+    with pytest.raises(NotImplementedError) as e:
+        wavread(filename)
+
+    assert str(e.value) == "Invalid header 0 is detected."
+
+    # invalid fmt byte rate
+    save_invalid_wavfile(filename, invalid_byte_rate=True)
+
+    with pytest.raises(ValueError) as e:
+        wavread(filename)
+
+    assert str(e.value) == "Invalid header is detected."
+
+    # invalid data chunk marker
+    save_invalid_wavfile(filename, invalid_data_chunk_marker=True)
+
+    with pytest.raises(NotImplementedError) as e:
+        wavread(filename)
+
+    assert str(e.value) == f"Not support {b'DATA'}."
+
+    os.remove(filename)
