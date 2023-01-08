8721e2e5cc19d1d6c46c2e06e99e3c2d92ae946a
tky823 <tky823.main+github@gmail.com>
2023-01-09 00:36:31 +0900

    [docs] Add docs of geometric mean

--- ssspy/linalg/mean.py
+++ ssspy/linalg/mean.py
@@ -15,6 +15,16 @@ def gmeanmh(A: np.ndarray, B: np.ndarray) -> np.ndarray:
         &= \boldsymbol{A}(\boldsymbol{A}^{-1}\boldsymbol{B})^{1/2} \\
         &= (\boldsymbol{A}\boldsymbol{B}^{-1})^{1/2}\boldsymbol{B}
 
+    .. note::
+        :math:`(\boldsymbol{A}^{-1}\boldsymbol{B})^{1/2}` is computed by
+        generalized eigendecomposition.
+        Let :math:`\lambda` and :math:`z` be the eigenvalue and eigenvector of
+        the generalized eigenproblem :math:`\boldsymbol{Bz}=\lambda\boldsymbol{Az}`.
+        Then, :math:`(\boldsymbol{A}^{-1}\boldsymbol{B})^{1/2}` is computed by
+        :math:`\boldsymbol{Z}\boldsymbol{\Lambda}^{1/2}\boldsymbol{Z}^{-1}`,
+        where the main diagonals of :math:`\boldsymbol{\Lambda}` are :math:`\lambda`s
+        and the columns of :math:`\boldsymbol{Z}` are :math:`\boldsymbol{z}`s.
+
     Args:
         A (numpy.ndarray):
             A complex Hermitian matrix with shape of (\*, n_channels, n_channels).
