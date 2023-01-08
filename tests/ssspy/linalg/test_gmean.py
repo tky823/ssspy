016c525e95d2b9a1aaf12feff7cc87229fbfe65c
tky823 <tky823.main+github@gmail.com>
2023-01-08 23:19:12 +0900

    [tests] Test batch operation of geometric mean

--- tests/ssspy/linalg/test_gmean.py
+++ tests/ssspy/linalg/test_gmean.py
@@ -5,13 +5,16 @@ from ssspy.linalg import gmeanmh
 
 
 def gmeanmh_scipy(A: np.ndarray, B: np.ndarray, inverse="left") -> np.ndarray:
+    def _sqrtm(X) -> np.ndarray:
+        return np.stack([sqrtm(x) for x in X], axis=0)
+
     if inverse == "left":
         AB = np.linalg.solve(A, B)
-        G = A @ sqrtm(AB)
+        G = A @ _sqrtm(AB)
     elif inverse == "right":
         AB = np.linalg.solve(B, A)
         AB = AB.swapaxes(-2, -1).conj()
-        G = sqrtm(AB) @ B
+        G = _sqrtm(AB) @ B
     else:
         raise ValueError(f"Invalid inverse={inverse} is given.")
 
@@ -20,12 +23,13 @@ def gmeanmh_scipy(A: np.ndarray, B: np.ndarray, inverse="left") -> np.ndarray:
 
 def test_gmean():
     rng = np.random.default_rng(0)
-    size = (16, 4, 1)
+    size = (16, 32, 4, 1)
 
     def create_psd():
         x = rng.random(size) + 1j * rng.random(size)
+        XX = x * x.transpose(0, 1, 3, 2).conj()
 
-        return np.mean(x * x.transpose(0, 2, 1).conj(), axis=0)
+        return np.mean(XX, axis=0)
 
     A = create_psd()
     B = create_psd()
