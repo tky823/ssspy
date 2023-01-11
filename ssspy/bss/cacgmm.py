975734f6bc6f778c0e3654e68e681ba6ba51d922
tky823 <tky823.main+github@gmail.com>
2023-01-11 23:50:22 +0900

    [src] Create cacgmm.py

---
+++ ssspy/bss/cacgmm.py
@@ -0,0 +1,125 @@
+from typing import Callable, List, Optional, Union
+
+import numpy as np
+
+from .base import IterativeMethodBase
+
+
+class CACGMMbase(IterativeMethodBase):
+    r"""Base class of independent low-rank matrix analysis (ILRMA).
+
+    Args:
+        n_sources (int, optional):
+            Number of sources to be separated.
+            If ``None`` is given, ``n_sources`` is determined by number of channels
+            in input spectrogram. Default: ``None``.
+        callbacks (callable or list[callable], optional):
+            Callback functions. Each function is called before separation and at each iteration.
+            Default: ``None``.
+        record_loss (bool):
+            Record the loss at each iteration of the update algorithm if ``record_loss=True``.
+            Default: ``True``.
+    """
+
+    def __init__(
+        self,
+        n_sources: Optional[int] = None,
+        callbacks: Optional[
+            Union[
+                Callable[["CACGMMbase"], None],
+                List[Callable[["CACGMMbase"], None]],
+            ]
+        ] = None,
+        record_loss: bool = True,
+    ) -> None:
+        super().__init__(callbacks=callbacks, record_loss=record_loss)
+
+        self.n_sources = n_sources
+
+    def __call__(
+        self, input: np.ndarray, n_iter: int = 100, initial_call: bool = True, **kwargs
+    ) -> np.ndarray:
+        r"""Separate a frequency-domain multichannel signal.
+
+        Args:
+            input (numpy.ndarray):
+                The mixture signal in frequency-domain.
+                The shape is (n_channels, n_bins, n_frames).
+            n_iter (int):
+                The number of iterations of demixing filter updates.
+                Default: ``100``.
+            initial_call (bool):
+                If ``True``, perform callbacks (and computation of loss if necessary)
+                before iterations.
+
+        Returns:
+            numpy.ndarray of the separated signal in frequency-domain.
+            The shape is (n_channels, n_bins, n_frames).
+        """
+        self.input = input.copy()
+
+        self._reset(**kwargs)
+
+        self.n_iter = n_iter
+
+        super().__call__(n_iter=n_iter, initial_call=initial_call)
+
+        self.output = self.separate(self.input)
+
+        return self.output
+
+    def _reset(self, **kwargs) -> None:
+        r"""Reset attributes by given keyword arguments.
+
+        Args:
+            kwargs:
+                Keyword arguments to set as attributes of CACGMM.
+        """
+        assert self.input is not None, "Specify data!"
+
+        for key in kwargs.keys():
+            setattr(self, key, kwargs[key])
+
+        X = self.input
+
+        norm = np.linalg.norm(X, axis=0)
+        Z = X / norm
+        self.unit_input = Z
+
+        n_sources = self.n_sources
+        n_channels, n_bins, n_frames = X.shape
+
+        if n_sources is None:
+            n_sources = n_channels
+
+        self.n_sources, self.n_channels = n_sources, n_channels
+        self.n_bins, self.n_frames = n_bins, n_frames
+
+        alpha = np.ones((n_sources, n_bins)) / n_sources
+        eye = np.eye(n_channels, dtype=np.complex128)
+        B = np.tile(eye, reps=(n_sources, n_bins, 1, 1))
+
+        self.mixing = alpha
+        self.covariance = B
+
+    def separate(self, input: np.ndarray) -> np.ndarray:
+        r"""Separate ``input``.
+
+        Args:
+            input (numpy.ndarray):
+                The mixture signal in frequency-domain.
+                The shape is (n_channels, n_bins, n_frames).
+
+        Returns:
+            numpy.ndarray of the separated signal in frequency-domain.
+            The shape is (n_sources, n_bins, n_frames).
+        """
+        raise NotImplementedError("Implement 'separate' method.")
+
+    def compute_loss(self) -> float:
+        r"""Compute loss :math:`\mathcal{L}`.
+
+        Returns:
+            Computed loss.
+        """
+        raise NotImplementedError("Implement 'compute_loss' method.")
