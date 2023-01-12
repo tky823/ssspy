7cdfeebae131451fba8ed35bf427ac2b04cb562a
tky823 <tky823.main+github@gmail.com>
2023-01-12 22:08:28 +0900

    [docs] Add examples of logsumexp

--- ssspy/special/logsumexp.py
+++ ssspy/special/logsumexp.py
@@ -16,6 +16,18 @@ def logsumexp(X: np.ndarray, axis: int = None, keepdims: bool = False) -> np.nda
 
     Returns:
         np.ndarray of log-sum-exp values.
+
+    Examples:
+
+        .. code-block:: python
+
+            >>> import numpy as np
+
+            >>> X = np.array([[1, 2, 3], [4, 5, 6]])
+            >>> logsumexp(X, axis=0)
+            array([4.04858735, 5.04858735, 6.04858735])
+            >>> logsumexp(X, axis=1)
+            array([3.40760596, 6.40760596])
     """
     vmax = np.max(X, axis=axis, keepdims=True)
     exp = np.exp(X - vmax)
