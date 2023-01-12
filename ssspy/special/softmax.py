649f6597e2e741b10fac29eb2795248b51442b52
tky823 <tky823.main+github@gmail.com>
2023-01-12 22:08:37 +0900

    [docs] Add examples of softmax

--- ssspy/special/softmax.py
+++ ssspy/special/softmax.py
@@ -13,6 +13,20 @@ def softmax(X: np.ndarray, axis: int = None) -> np.ndarray:
 
     Returns:
         np.ndarray of softmax values.
+
+    Examples:
+
+        .. code-block:: python
+
+            >>> import numpy as np
+
+            >>> X = np.array([[1, 2, 3], [4, 5, 6]])
+            >>> softmax(X, axis=0)
+            array([[0.04742587, 0.04742587, 0.04742587],
+                [0.95257413, 0.95257413, 0.95257413]])
+            >>> softmax(X, axis=1)
+            array([[0.09003057, 0.24472847, 0.66524096],
+                [0.09003057, 0.24472847, 0.66524096]])
     """
     vmax = np.max(X, axis=axis, keepdims=True)
     Y = X - vmax
