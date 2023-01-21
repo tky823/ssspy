7d962ea50d8f6ce35760cd09ca1ef224144ba096
tky823 <tky823.main+github@gmail.com>
2023-01-21 21:42:54 +0900

    [tests] Fix wrappers for warnings

--- tests/ssspy/bss/test_pair_selector.py
+++ tests/ssspy/bss/test_pair_selector.py
@@ -11,16 +11,20 @@ parameters_ascend = [True, False]
 @pytest.mark.parametrize("step", parameters_step)
 @pytest.mark.parametrize("ascend", parameters_ascend)
 def test_sequential_pair_selector(n_sources: int, step: int, ascend: bool):
-    with pytest.raises(UserWarning) as e:
+    with pytest.warns(UserWarning) as record:
         sequential_pair_selector(n_sources, step=step, sort=ascend)
 
-    assert str(e) == "Use ssspy.utils.select_pair.sequential_pair_selector instead."
+    assert len(record) == 1
+    assert str(record[0].message) == "Use ssspy.utils.select_pair.sequential_pair_selector instead."
 
 
 @pytest.mark.parametrize("n_sources", parameters_n_sources)
 @pytest.mark.parametrize("ascend", parameters_ascend)
 def test_combination_pair_selector(n_sources: int, ascend: bool):
-    with pytest.raises(UserWarning) as e:
+    with pytest.warns(UserWarning) as record:
         combination_pair_selector(n_sources, sort=ascend)
 
-    assert str(e) == "Use ssspy.utils.select_pair.combination_pair_selector instead."
+    assert len(record) == 1
+    assert (
+        str(record[0].message) == "Use ssspy.utils.select_pair.combination_pair_selector instead."
+    )
