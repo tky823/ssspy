637ac10d2742a7e34ee9b3c3a31be05e801c2ca1
tky823 <tky823.main+github@gmail.com>
2023-02-05 17:27:31 +0900

    [src] Add identity to __all__

--- ssspy/special/__init__.py
+++ ssspy/special/__init__.py
@@ -1,5 +1,5 @@
-from .flooring import add_flooring, max_flooring
+from .flooring import add_flooring, identity, max_flooring
 from .logsumexp import logsumexp
 from .softmax import softmax
 
-__all__ = ["add_flooring", "max_flooring", "logsumexp", "softmax"]
+__all__ = ["add_flooring", "max_flooring", "identity", "logsumexp", "softmax"]
