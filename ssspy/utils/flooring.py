525fb35e5df7baec717e34fe90134eae86ea289a
tky823 <tky823.main+github@gmail.com>
2023-02-05 15:07:28 +0900

    [src] Add assertion to examine method

--- ssspy/utils/flooring.py
+++ ssspy/utils/flooring.py
@@ -6,16 +6,18 @@ from ..special.flooring import identity
 
 
 def choose_flooring_fn(
-    method: Any,
     flooring_fn: Optional[Union[str, Callable[[np.ndarray], np.ndarray]]] = "self",
+    method: Optional[Any] = None,
 ) -> Callable[[np.ndarray], np.ndarray]:
     if flooring_fn is None:
+        assert method is None, "method is given, but flooring function is not specified."
+
         flooring_fn = identity
     elif type(flooring_fn) is str and flooring_fn == "self":
-        if hasattr(method, "flooring_fn"):
-            flooring_fn = method.flooring_fn
-        else:
+        if method is None or not hasattr(method, "flooring_fn"):
             flooring_fn = identity
+        else:
+            flooring_fn = method.flooring_fn
 
     assert callable(flooring_fn), "flooring_fn should be callable."
 
