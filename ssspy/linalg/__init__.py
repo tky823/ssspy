a43f91d6a6d9de618464a8db4b0d74c925c9bbb9
tky823 <tky823.main+github@gmail.com>
2023-02-09 23:58:22 +0900

    [src] Add cbrt to __all__

--- ssspy/linalg/__init__.py
+++ ssspy/linalg/__init__.py
@@ -1,7 +1,17 @@
+from .cubic import cbrt
 from .eigh import eigh, eigh2
 from .inv import inv2
 from .mean import gmeanmh
 from .quadratic import quadratic
 from .sqrtm import invsqrtmh, sqrtmh
 
-__all__ = ["quadratic", "inv2", "eigh", "eigh2", "sqrtmh", "invsqrtmh", "gmeanmh"]
+__all__ = [
+    "cbrt",
+    "quadratic",
+    "inv2",
+    "eigh",
+    "eigh2",
+    "sqrtmh",
+    "invsqrtmh",
+    "gmeanmh",
+]
