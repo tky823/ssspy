5a203bdf7160093371844ec252715445d7db6919
tky823 <tky823.main+github@gmail.com>
2023-02-10 08:48:39 +0900

    [tests] Add test for cbrt with complex number

--- tests/ssspy/linalg/test_cubic.py
+++ tests/ssspy/linalg/test_cubic.py
@@ -8,7 +8,14 @@ def test_cbrt():
     n_bins = 8
     n_channels = 4
 
+    # real number
     x = rng.standard_normal((n_bins, n_channels))
+    y = cbrt(x)
+
+    assert np.allclose(y**3, x)
+
+    # complex number
+    x = rng.standard_normal((n_bins, n_channels)) + 1j * rng.standard_normal((n_bins, n_channels))
     y = cbrt(x)
 
     assert np.allclose(y**3, x)
