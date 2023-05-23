c2a0ad782e40c1b87722b2d2136f79fa161effee
tky823 <tky823.main+github@gmail.com>
2023-05-23 23:37:52 +0900

    [tests] Download datasets in advance

---
+++ tests/scripts/download_all.py
@@ -0,0 +1,14 @@
+from dummy.utils.dataset import download_sample_speech_data
+
+
+def download_all() -> None:
+    conditions = [
+        {"n_sources": 3, "sisec2010_tag": "dev1_female3"},
+        {"n_sources": 4, "sisec2010_tag": "dev1_female4"},
+    ]
+    for kwargs in conditions:
+        download_sample_speech_data(**kwargs)
+
+
+if __name__ == "__main__":
+    download_all()
