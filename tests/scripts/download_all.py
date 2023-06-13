093fe7a3919f20488041bc0a3230cf49acf63fe7
tky823 <tky823.main+github@gmail.com>
2023-06-14 00:53:00 +0900

    [tests] Download download_ssspy_data in advance

--- tests/scripts/download_all.py
+++ tests/scripts/download_all.py
@@ -6,9 +6,11 @@ tests_dir = dirname(dirname(realpath(__file__)))
 sys.path.append(tests_dir)
 
 from dummy.utils.dataset import download_sample_speech_data  # noqa: E402
+from dummy.utils.dataset import download_ssspy_data  # noqa: E402
 
 
 def download_all() -> None:
+    # Download sample speech data
     conditions = [
         {"n_sources": 2, "sisec2010_tag": "dev1_female3"},
         {"n_sources": 3, "sisec2010_tag": "dev1_female3"},
@@ -19,6 +21,17 @@ def download_all() -> None:
     for kwargs in conditions:
         for max_duration in max_durations:
             download_sample_speech_data(max_duration=max_duration, **kwargs)
+
+    # Download sample audio for tests of IO
+    paths = [
+        "audio/monoral_16k_5sec.wav",
+        "audio/stereo_16k_5sec.wav",
+    ]
+    template_filename = "./tests/mock/{}"
+
+    for path in paths:
+        filename = template_filename.format(path)
+        download_ssspy_data(path, filename=filename)
 
 
 if __name__ == "__main__":
