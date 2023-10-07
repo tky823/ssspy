eabceac6c830bb501cf79fcf0495697ff6dabc4e
tky823 <tky823.main+github@gmail.com>
2023-10-07 13:05:32 +0900

    [src] Support 2D input for _kummer

--- ssspy/bss/cwmm.py
+++ ssspy/bss/cwmm.py
@@ -168,19 +168,33 @@ def _kummer(kappa: np.ndarray) -> np.ndarray:
     """Kummer function M(1, N; X).
 
     Args:
-        kappa (np.ndarray): Concentration parameter of shape (n_sources,).
+        kappa (np.ndarray): Concentration parameter of shape (n_sources,)
+            or (n_sources, n_bins).
 
     Returns:
-        np.ndarray of computed values of shape (n_sources,).
+        np.ndarray of computed values of same shape as kappa.
 
     """
+    ndim = kappa.ndim
+
+    if ndim == 1:
+        kappa = kappa[:, np.newaxis]
+    elif ndim == 2:
+        pass
+    else:
+        raise ValueError("kappa should be 1D or 2D.")
+
     n_sources = kappa.shape[0]
     indices = np.arange(1, n_sources)
     cumprod = np.cumprod(indices, axis=0)
-    kappa_l = kappa ** indices[:, np.newaxis]
+
+    kappa_l = kappa ** indices[:, np.newaxis, np.newaxis]
     scale = cumprod[-1] / kappa_l[-1]
     exp_kappa = np.exp(kappa)
-    terms = kappa_l / cumprod[:, np.newaxis]
+    terms = kappa_l / cumprod[:, np.newaxis, np.newaxis]
     k = scale * (exp_kappa - np.sum(terms[:-1], axis=0) - 1)
+
+    if ndim == 1:
+        k = k.squeeze(axis=-1)
 
     return k
