94454a0ca11706526ea998773c09e624109bce05
tky823 <tky823.main+github@gmail.com>
2023-10-13 01:11:40 +0900

    [tests] Save feature in test of IVA if necessary

--- tests/regression/bss/test_iva.py
+++ tests/regression/bss/test_iva.py
@@ -13,13 +13,19 @@ from dummy.utils.dataset import load_regression_data  # noqa: E402
 iva_root = join(ssspy_tests_dir, "mock", "regression", "bss", "iva")
 
 
-def test_aux_iva():
+def test_aux_iva(save_feature: bool = False):
     root = join(iva_root, "aux_iva")
 
-    npz_input, npz_target = load_regression_data(root, filenames=["input.npz", "target.npz"])
+    if save_feature:
+        (npz_input,) = load_regression_data(root=root, filenames=["input.npz"])
+        spectrogram_tgt = None
+        n_iter = 10
+    else:
+        npz_input, npz_target = load_regression_data(root, filenames=["input.npz", "target.npz"])
+        spectrogram_tgt = npz_target["spectrogram"]
+        n_iter = npz_target["n_iter"].item()
+
     spectrogram_mix = npz_input["spectrogram"]
-    spectrogram_tgt = npz_target["spectrogram"]
-    n_iter = npz_target["n_iter"].item()
 
     def contrast_fn(y: np.ndarray) -> np.ndarray:
         r"""Contrast function.
@@ -50,4 +56,19 @@ def test_aux_iva():
     iva = AuxIVA(contrast_fn=contrast_fn, d_contrast_fn=d_contrast_fn)
     spectrogram_est = iva(spectrogram_mix, n_iter=n_iter)
 
-    assert np.allclose(spectrogram_est, spectrogram_tgt)
+    if save_feature:
+        np.savez(
+            join(root, "target.npz"),
+            spectrogram=spectrogram_est,
+            n_iter=n_iter,
+        )
+    else:
+        assert np.allclose(spectrogram_est, spectrogram_tgt)
+
+
+def save_all_features() -> None:
+    test_aux_iva(save_feature=True)
+
+
+if __name__ == "__main__":
+    save_all_features()
