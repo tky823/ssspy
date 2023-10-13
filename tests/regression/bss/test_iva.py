05437455db115407353aed34c77bdbe334a55169
tky823 <tky823.main+github@gmail.com>
2023-10-13 23:07:14 +0900

    [tests] Add regression test for FastIVA

--- tests/regression/bss/test_iva.py
+++ tests/regression/bss/test_iva.py
@@ -5,7 +5,7 @@ from os.path import dirname, join, realpath
 import numpy as np
 import pytest
 
-from ssspy.bss.iva import AuxIVA, GradIVA, NaturalGradIVA
+from ssspy.bss.iva import AuxIVA, FastIVA, GradIVA, NaturalGradIVA
 
 ssspy_tests_dir = dirname(dirname(dirname(realpath(__file__))))
 sys.path.append(ssspy_tests_dir)
@@ -211,6 +211,79 @@ def test_aux_iva(spatial_algorithm: str, save_feature: bool = False):
         )
 
 
+def test_fast_iva(save_feature: bool = False):
+    root = join(iva_root, "fast_iva")
+
+    if save_feature:
+        (npz_input,) = load_regression_data(root=root, filenames=["input.npz"])
+        spectrogram_tgt = None
+        n_iter = 5
+    else:
+        npz_input, npz_target = load_regression_data(root, filenames=["input.npz", "target.npz"])
+        spectrogram_tgt = npz_target["spectrogram"]
+        n_iter = npz_target["n_iter"].item()
+
+    spectrogram_mix = npz_input["spectrogram"]
+
+    def contrast_fn(y: np.ndarray) -> np.ndarray:
+        r"""Contrast function.
+
+        Args:
+            y (np.ndarray):
+                The shape is (n_sources, n_bins, n_frames).
+
+        Returns:
+            np.ndarray:
+                The shape is (n_sources, n_frames).
+        """
+        return 2 * np.linalg.norm(y, axis=1)
+
+    def d_contrast_fn(y) -> np.ndarray:
+        r"""Derivative of contrast function.
+
+        Args:
+            y (np.ndarray):
+                The shape is (n_sources, n_frames).
+
+        Returns:
+            np.ndarray:
+                The shape is (n_sources, n_frames).
+        """
+        return 2 * np.ones_like(y)
+
+    def dd_contrast_fn(y) -> np.ndarray:
+        r"""Second roder derivative of contrast function.
+
+        Args:
+            y (np.ndarray):
+                The shape is (n_sources, n_frames).
+
+        Returns:
+            np.ndarray:
+                The shape is (n_sources, n_frames).
+        """
+        return 2 * np.zeros_like(y)
+
+    iva = FastIVA(
+        contrast_fn=contrast_fn,
+        d_contrast_fn=d_contrast_fn,
+        dd_contrast_fn=dd_contrast_fn,
+    )
+    spectrogram_est = iva(spectrogram_mix, n_iter=n_iter)
+
+    if save_feature:
+        makedirs(root, exist_ok=True)
+        np.savez(
+            join(root, "target.npz"),
+            spectrogram=spectrogram_est,
+            n_iter=n_iter,
+        )
+    else:
+        assert np.allclose(spectrogram_est, spectrogram_tgt, atol=1e-7), np.max(
+            np.abs(spectrogram_est - spectrogram_tgt)
+        )
+
+
 def save_all_features() -> None:
     for is_holonomic in parameters_is_holonomic:
         test_grad_iva(is_holonomic=is_holonomic, save_feature=True)
@@ -220,6 +293,8 @@ def save_all_features() -> None:
 
     for spatial_algorithm in parameters_spatial_algorithm:
         test_aux_iva(spatial_algorithm=spatial_algorithm, save_feature=True)
+
+    test_fast_iva(save_feature=True)
 
 
 if __name__ == "__main__":
